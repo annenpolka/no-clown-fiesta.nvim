48739570beb7383ea86e6c534e1640d1969a29e4
annnenpolka <k.miyakewiss1@gmail.com>
2022-02-19 17:11:27 +0900

    add GitGutter~ colors

--- lua/no-clown-fiesta/Git.lua
+++ lua/no-clown-fiesta/Git.lua
@@ -5,6 +5,9 @@ local Git = {
   GitSignsAdd = { fg = C.sign_add },
   GitSignsChange = { fg = C.sign_change },
   GitSignsDelete = { fg = C.sign_delete },
+  GitGutterAdd = { fg = C.sign_add },
+  GitGutterChange = { fg = C.sign_change },
+  GitGutterDelete = { fg = C.sign_delete },
 }
 
 return Git
